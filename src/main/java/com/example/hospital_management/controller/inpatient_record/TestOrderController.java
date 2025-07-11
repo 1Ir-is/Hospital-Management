eaa6508b0ceba9d095ec80310c8dc2c661c9a10e
Chung128 <tranvanchung120897@gmail.com>
2025-07-11 11:51:00 +0700

    luu code

--- src/main/java/com/example/hospital_management/controller/inpatient_record/TestOrderController.java
+++ src/main/java/com/example/hospital_management/controller/inpatient_record/TestOrderController.java
@@ -1,11 +1,9 @@
-package com.example.hospital_management.controller;
+package com.example.hospital_management.controller.inpatient_record;
 
 
-import com.example.hospital_management.dto.TestOrderDTO;
 import com.example.hospital_management.entity.*;
 import com.example.hospital_management.service.*;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,15 +13,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/doctor")
-public class TestOrderController {
+public class  TestOrderController {
     private ITestOrderService testOrderService;
     private IEmployeeService employeeService;
     private ITestService testService;
